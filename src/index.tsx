116029c71d588eddbe77da49926cea31d346d8f9
Olivia Aldisa <oliviaaldisa@hotmail.com>
2020-09-23 10:02:31 +0700

    add packag

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import PriusCircularProgress from './ui_palette/PriusCircularProgress';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import * as serviceWorker from "./serviceWorker";
+// import PriusCircularProgress from "./ui_palette/PriusCircularProgress";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <PriusCircularProgress />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <PriusCircularProgress />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+import PriusBackdrop from "./ui_palette/PriusBackdrop";
+import PriusBrandDropdown from "./ui_palette/PriusBrandDropdown";
+import {
+  GetBrandsResponseType,
+  GetBrandsParamType,
+} from "./ui_palette/PriusBrandDropdown/types";
+import PriusButtonText from "./ui_palette/PriusButtonText";
+import PriusCard from "./ui_palette/PriusCard";
+import PriusCardDND from "./ui_palette/PriusCardDND";
+import PriusCardTemplate from "./ui_palette/PriusCardTemplate";
+import PriusCheckbox from "./ui_palette/PriusCheckbox";
+import PriusCircularProgress from "./ui_palette/PriusCircularProgress";
+import PriusColorPicker from "./ui_palette/PriusColorPicker";
+import PriusConfirmDialog from "./ui_palette/PriusConfirmDialog";
+import PriusDatePickerInput from "./ui_palette/PriusDatePickerInput";
+import PriusDateRangeInput from "./ui_palette/PriusDateRangeInput";
+import PriusFieldset from "./ui_palette/PriusFieldset";
+import PriusFilter from "./ui_palette/PriusFilter";
+import { FilterCss } from "./ui_palette/PriusFilter/FilterCss";
+import { FilterItemType } from "./ui_palette/PriusFilter/types";
+import PriusFooter from "./ui_palette/PriusFooter";
+import PriusImageUploader from "./ui_palette/PriusImageUploader";
+import { StatusUploadType } from "./ui_palette/PriusIndicatorLabel";
+import PriusInfoBox from "./ui_palette/PriusInfoBox";
+import PriusInputGroup from "./ui_palette/PriusInputGroup";
+import PriusInput from "./ui_palette/PriusInputNew";
+import PriusLabel from "./ui_palette/PriusLabel";
+import PriusMenu from "./ui_palette/PriusMenu/MainMenu";
+import PriusMultipleImage from "./ui_palette/PriusMultipleImage";
+import PriusMultipleSelect, { OptionType } from "./ui_palette/PriusMultipleSelect";
+import PriusNavigationBar from "./ui_palette/PriusNavigationBar/NavigationBar";
+import PriusOption from "./ui_palette/PriusOption";
+import PriusPaper from "./ui_palette/PriusPaper";
+import PriusPrintPage from "./ui_palette/PriusPrintPage";
+import PriusSelect from "./ui_palette/PriusSelect";
+import PriusSEO from "./ui_palette/PriusSEO";
+import { SEOType } from "./ui_palette/PriusSEO/types";
+import { TenantContext } from "./ui_palette/PriusSSSO";
+import PriusTab from "./ui_palette/PriusTab";
+import PriusTable, {
+  FieldToggle,
+  FieldAction,
+  FieldText,
+  ROWS_PER_PAGE,
+} from "./ui_palette/PriusTable";
+import PriusTablePagination from "./ui_palette/PriusTable/TablePagination";
+import PriusTabPanel from "./ui_palette/PriusTabPanel";
+import PriusTextbox from "./ui_palette/PriusTextbox/PriusTextbox";
+import PriusTextEditor from "./ui_palette/PriusTextEditor";
+import PriusThumbnail from "./ui_palette/PriusThumbnail";
+import FallbackImage from "./ui_palette/PriusThumbnail/FallbackImage.svg";
+import PriusTitle from "./ui_palette/PriusTitle";
+import PriusToggle from "./ui_palette/PriusToggle";
+import PriusTooltip from "./ui_palette/PriusTooltip";
+import PriusUploadFile from "./ui_palette/PriusUploadFile/PriusUploadFile";
+import {
+  useSnackbar,
+  withSnackbar,
+  SnackbarContextType,
+} from "./ui_palette/PriusSnackbar";
+
+export {
+  FallbackImage,
+  FieldAction,
+  FieldText,
+  FieldToggle,
+  FilterCss,
+  PriusBackdrop,
+  PriusBrandDropdown,
+  PriusButtonText,
+  PriusCard,
+  PriusCardDND,
+  PriusCardTemplate,
+  PriusCheckbox,
+  PriusCircularProgress,
+  PriusColorPicker,
+  PriusConfirmDialog,
+  PriusDatePickerInput,
+  PriusDateRangeInput,
+  PriusFieldset,
+  PriusFilter,
+  PriusFooter,
+  PriusImageUploader,
+  PriusInfoBox,
+  PriusInput,
+  PriusInputGroup,
+  PriusLabel,
+  PriusMenu,
+  PriusMultipleImage,
+  PriusMultipleSelect,
+  PriusNavigationBar,
+  PriusOption,
+  PriusPaper,
+  PriusPrintPage,
+  PriusSelect,
+  PriusSEO,
+  PriusTab,
+  PriusTable,
+  PriusTablePagination,
+  PriusTabPanel,
+  PriusTextbox,
+  PriusTextEditor,
+  PriusThumbnail,
+  PriusTitle,
+  PriusToggle,
+  PriusTooltip,
+  PriusUploadFile,
+  ROWS_PER_PAGE,
+  StatusUploadType,
+  TenantContext,
+  useSnackbar,
+  withSnackbar,
+};
