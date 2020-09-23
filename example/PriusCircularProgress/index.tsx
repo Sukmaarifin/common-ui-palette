0a5bc805fe9a8aecf71feadff869cc5268e05f4d
Sukma Arifin <sukmarifin@gmail.com>
2020-09-23 15:46:26 +0700

    chore: test eslint

---
+++ example/PriusCircularProgress/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PriusCircularProgress from '../../src/ui_palette/PriusCircularProgress';
+import PriusColorPicker from '../../src/ui_palette/PriusColorPicker';
+
+const PriusCircularProgressExample = () => {
+
+    return (
+        <PriusCircularProgress/>
+    )
+}
+
+export default PriusCircularProgressExample;
