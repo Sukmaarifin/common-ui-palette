ba3b9214d06a152fd5b6630f2f8c1c9e279a56c2
Annisa Feryannie <aferyannie@gmail.com>
2020-09-24 13:56:10 +0700

    [ui_palette] update usage

--- index.tsx
+++ index.tsx
@@ -1,125 +1,125 @@
-export { default as Backdrop } from "./src/ui_palette/PriusBackdrop";
-export * from "./src/ui_palette/PriusBackdrop";
+export { default as Backdrop } from './src/components/PriusBackdrop';
+export * from './src/components/PriusBackdrop';
 
-export { default as BrandDropdown } from "./src/ui_palette/PriusBrandDropdown";
-export * from "./src/ui_palette/PriusBrandDropdown";
+export { default as BrandDropdown } from './src/components/PriusBrandDropdown';
+export * from './src/components/PriusBrandDropdown';
 
-export { default as ButtonText } from "./src/ui_palette/PriusButtonText";
-export * from "./src/ui_palette/PriusButtonText";
+export { default as ButtonText } from './src/components/PriusButtonText';
+export * from './src/components/PriusButtonText';
 
-export { default as Card } from "./src/ui_palette/PriusCard";
-export * from "./src/ui_palette/PriusCard";
+export { default as Card } from './src/components/PriusCard';
+export * from './src/components/PriusCard';
 
-export { default as CardDND } from "./src/ui_palette/PriusCardDND";
-export * from "./src/ui_palette/PriusCardDND";
+export { default as CardDND } from './src/components/PriusCardDND';
+export * from './src/components/PriusCardDND';
 
-export { default as CardTemplate } from "./src/ui_palette/PriusCardTemplate";
-export * from "./src/ui_palette/PriusCardTemplate";
+export { default as CardTemplate } from './src/components/PriusCardTemplate';
+export * from './src/components/PriusCardTemplate';
 
-export { default as Checkbox } from "./src/ui_palette/PriusCheckbox";
-export * from "./src/ui_palette/PriusCheckbox";
+export { default as Checkbox } from './src/components/PriusCheckbox';
+export * from './src/components/PriusCheckbox';
 
-export { default as CircularProgress } from "./src/ui_palette/PriusCircularProgress";
-export * from "./src/ui_palette/PriusCircularProgress";
+export { default as CircularProgress } from './src/components/PriusCircularProgress';
+export * from './src/components/PriusCircularProgress';
 
-export { default as ColorPicker } from "./src/ui_palette/PriusColorPicker";
-export * from "./src/ui_palette/PriusColorPicker";
+export { default as ColorPicker } from './src/components/PriusColorPicker';
+export * from './src/components/PriusColorPicker';
 
-export { default as ConfirmDialog } from "./src/ui_palette/PriusConfirmDialog";
-export * from "./src/ui_palette/PriusConfirmDialog";
+export { default as ConfirmDialog } from './src/components/PriusConfirmDialog';
+export * from './src/components/PriusConfirmDialog';
 
-export { default as DatePicker } from "./src/ui_palette/PriusDatePicker";
-export * from "./src/ui_palette/PriusDatePicker";
+export { default as DatePicker } from './src/components/PriusDatePicker';
+export * from './src/components/PriusDatePicker';
 
-export { default as DatePickerInput } from "./src/ui_palette/PriusDatePickerInput";
-export * from "./src/ui_palette/PriusDatePickerInput";
+export { default as DatePickerInput } from './src/components/PriusDatePickerInput';
+export * from './src/components/PriusDatePickerInput';
 
-export { default as DateRangeInput } from "./src/ui_palette/PriusDateRangeInput";
-export * from "./src/ui_palette/PriusDateRangeInput";
+export { default as DateRangeInput } from './src/components/PriusDateRangeInput';
+export * from './src/components/PriusDateRangeInput';
 
-export { default as DraggableImage } from "./src/ui_palette/PriusDraggableImage";
-export * from "./src/ui_palette/PriusDraggableImage";
+export { default as DraggableImage } from './src/components/PriusDraggableImage';
+export * from './src/components/PriusDraggableImage';
 
-export { default as Fieldset } from "./src/ui_palette/PriusFieldset";
-export * from "./src/ui_palette/PriusFieldset";
+export { default as Fieldset } from './src/components/PriusFieldset';
+export * from './src/components/PriusFieldset';
 
-export { default as Filter } from "./src/ui_palette/PriusFilter";
-export * from "./src/ui_palette/PriusFilter";
+export { default as Filter } from './src/components/PriusFilter';
+export * from './src/components/PriusFilter';
 
-export { default as Footer } from "./src/ui_palette/PriusFilter";
-export * from "./src/ui_palette/PriusFilter";
+export { default as Footer } from './src/components/PriusFilter';
+export * from './src/components/PriusFilter';
 
-export { default as FileUploader } from "./src/ui_palette/PriusUploadFile";
-export * from "./src/ui_palette/PriusUploadFile";
+export { default as FileUploader } from './src/components/PriusUploadFile';
+export * from './src/components/PriusUploadFile';
 
-export { default as ImageUploader } from "./src/ui_palette/PriusImageUploader";
-export * from "./src/ui_palette/PriusImageUploader";
+export { default as ImageUploader } from './src/components/PriusImageUploader';
+export * from './src/components/PriusImageUploader';
 
-export { default as IndicatorLabel } from "./src/ui_palette/PriusIndicatorLabel";
-export * from "./src/ui_palette/PriusIndicatorLabel";
+export { default as IndicatorLabel } from './src/components/PriusIndicatorLabel';
+export * from './src/components/PriusIndicatorLabel';
 
-export { default as InfoBox } from "./src/ui_palette/PriusInfoBox";
-export * from "./src/ui_palette/PriusInfoBox";
+export { default as InfoBox } from './src/components/PriusInfoBox';
+export * from './src/components/PriusInfoBox';
 
-export { default as Input } from "./src/ui_palette/PriusInputNew";
-export * from "./src/ui_palette/PriusInputNew";
+export { default as Input } from './src/components/PriusInputNew';
+export * from './src/components/PriusInputNew';
 
-export { default as InputGroup } from "./src/ui_palette/PriusInputGroup";
-export * from "./src/ui_palette/PriusInputGroup";
+export { default as InputGroup } from './src/components/PriusInputGroup';
+export * from './src/components/PriusInputGroup';
 
-export { default as Label } from "./src/ui_palette/PriusLabel";
-export * from "./src/ui_palette/PriusLabel";
+export { default as Label } from './src/components/PriusLabel';
+export * from './src/components/PriusLabel';
 
-export { default as Menu } from "./src/ui_palette/PriusMenu";
-export * from "./src/ui_palette/PriusMenu";
+export { default as Menu } from './src/components/PriusMenu';
+export * from './src/components/PriusMenu';
 
-export { default as MultipleImage } from "./src/ui_palette/PriusMultipleImage";
-export * from "./src/ui_palette/PriusMultipleImage";
+export { default as MultipleImage } from './src/components/PriusMultipleImage';
+export * from './src/components/PriusMultipleImage';
 
-export { default as MultipleSelect } from "./src/ui_palette/PriusMultipleSelect";
-export * from "./src/ui_palette/PriusMultipleSelect";
+export { default as MultipleSelect } from './src/components/PriusMultipleSelect';
+export * from './src/components/PriusMultipleSelect';
 
-export { default as NavigationBar } from "./src/ui_palette/PriusNavigationBar";
-export * from "./src/ui_palette/PriusNavigationBar";
+export { default as NavigationBar } from './src/components/PriusNavigationBar';
+export * from './src/components/PriusNavigationBar';
 
-export { default as Option } from "./src/ui_palette/PriusOption";
-export * from "./src/ui_palette/PriusOption";
+export { default as Option } from './src/components/PriusOption';
+export * from './src/components/PriusOption';
 
-export { default as Paper } from "./src/ui_palette/PriusPaper";
-export * from "./src/ui_palette/PriusPaper";
+export { default as Paper } from './src/components/PriusPaper';
+export * from './src/components/PriusPaper';
 
-export { default as PrintPage } from "./src/ui_palette/PriusPrintPage";
-export * from "./src/ui_palette/PriusPrintPage";
+export { default as PrintPage } from './src/components/PriusPrintPage';
+export * from './src/components/PriusPrintPage';
 
-export { default as Router } from "./src/ui_palette/PriusRouter";
-export * from "./src/ui_palette/PriusRouter";
+export { default as Router } from './src/components/PriusRouter';
+export * from './src/components/PriusRouter';
 
-export { default as SEO } from "./src/ui_palette/PriusSEO";
-export * from "./src/ui_palette/PriusSEO";
+export { default as SEO } from './src/components/PriusSEO';
+export * from './src/components/PriusSEO';
 
-export { default as Select } from "./src/ui_palette/PriusSelect";
-export * from "./src/ui_palette/PriusSelect";
+export { default as Select } from './src/components/PriusSelect';
+export * from './src/components/PriusSelect';
 
-export { default as Tab } from "./src/ui_palette/PriusTab";
-export * from "./src/ui_palette/PriusTab";
+export { default as Tab } from './src/components/PriusTab';
+export * from './src/components/PriusTab';
 
-export { default as Table } from "./src/ui_palette/PriusTable";
-export * from "./src/ui_palette/PriusTable";
+export { default as Table } from './src/components/PriusTable';
+export * from './src/components/PriusTable';
 
-export { default as TabPanel } from "./src/ui_palette/PriusTabPanel";
-export * from "./src/ui_palette/PriusTabPanel";
+export { default as TabPanel } from './src/components/PriusTabPanel';
+export * from './src/components/PriusTabPanel';
 
-export { default as Textbox } from "./src/ui_palette/PriusTextbox";
-export * from "./src/ui_palette/PriusTextbox";
+export { default as Textbox } from './src/components/PriusTextbox';
+export * from './src/components/PriusTextbox';
 
-export { default as TextEditor } from "./src/ui_palette/PriusTextEditor";
-export * from "./src/ui_palette/PriusTextEditor";
+export { default as TextEditor } from './src/components/PriusTextEditor';
+export * from './src/components/PriusTextEditor';
 
-export { default as Thumbnail } from "./src/ui_palette/PriusThumbnail";
-export * from "./src/ui_palette/PriusThumbnail";
+export { default as Thumbnail } from './src/components/PriusThumbnail';
+export * from './src/components/PriusThumbnail';
 
-export { default as Tooltip } from "./src/ui_palette/PriusTooltip";
-export * from "./src/ui_palette/PriusTooltip";
+export { default as Tooltip } from './src/components/PriusTooltip';
+export * from './src/components/PriusTooltip';
 
-export { default as Toggle } from "./src/ui_palette/PriusToggle";
-export * from "./src/ui_palette/PriusToggle";
+export { default as Toggle } from './src/components/PriusToggle';
+export * from './src/components/PriusToggle';
